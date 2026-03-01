d4db7beb7c98914239f3dd57a25c90d7abc817e2
priyanka-01234 <chavhanpriya93@gmail.com>
2026-03-01 21:48:43 +0530

    new changes

--- RemoveDuplicateStringchar.java
+++ RemoveDuplicateStringchar.java
@@ -1,18 +1,36 @@
 package stringPrograms;
 
+import java.util.LinkedHashSet;
+
 public class RemoveDuplicateStringchar {
 
 	public static void main(String[] args) {
 		String s = "Hello";
 		String result = "";
-		for(int i=0; i<s.length(); i++) {
-			char ch =s.charAt(i);
+		LinkedHashSet<Character> set =  new LinkedHashSet();
+		
+		char[] ch  = s.toCharArray();
+		for(Character c : ch) {
+			set.add(c);
+		}
+		
+		System.out.println(set);
+		
+		for(char duplicate : set) {
+			result = result+duplicate;
 			
-			if (result.indexOf(ch) == -1) {
+		}
+		/*String result = "";
+		for(int i=0; i<s.length(); i++) {
+			char ch =s.charAt(i);	
+			
+		/*	if (result.indexOf(ch) == -1) {
 				result = result + ch;
 			}
-		}
+		}*/
 		System.out.println("String after removing duplicate characters: " + result);
+		
+		
 	}
 
 }
